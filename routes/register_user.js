93a7bd217ee63fc8e1b8f1650e9285d6dbfeba24
Mark Harrell <mark@mjharrell.com>
2014-11-12 18:06:34 -0800

    remove merge conflicts

--- routes/register_user.js
+++ routes/register_user.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-'use strict';
 var User = require('../models/user_model');
 var authController = require('../lib/auth');
-=======
-var User = require('../models/user');
-var authController = require('../controllers/auth');
->>>>>>> 1c4d4c3541578e03af7d3fb6b9f11513f77794e7
 
 module.exports = function(app) {
   app.post('/users', function(req, res) {
@@ -13,7 +7,7 @@ module.exports = function(app) {
       username: req.body.username,
       password: req.body.password
     });
-<<<<<<< HEAD
+
     user.save(function(err, data) {
       if (err) return res.send(err);
       res.json({'jwt': user.generateToken(app.get('jwtSecret'))});
@@ -21,12 +15,6 @@ module.exports = function(app) {
   });
   app.get('/users', authController.isAuthenticated, function(req, res) {
     res.json({'jwt': req.user.generateToken(app.get('jwtSecret'))});
-=======
-    user.save(function(err) {
-      if (err) res.send(err);
 
-      res.json({message: 'New note user added!'});
-    });
->>>>>>> 1c4d4c3541578e03af7d3fb6b9f11513f77794e7
   });
 };
