befd76e345b7197a18850ed5c4636283d115c410
Mark Harrell <mark@mjharrell.com>
2014-11-14 11:42:51 -0800

    confirm wip

--- routes/confirm_user.js
+++ routes/confirm_user.js
@@ -4,11 +4,7 @@ var authController = require('../lib/auth');
 module.exports = function(app, jwtauth) {
   app.post('/confirm', jwtauth, function(req, res) {
     var confirmation = {confirmationCode: req.confirmationCode};
-    User.findOne({'_id': jwtauth.decrypted.issuer}, function(err, data) {
-      if (err) return res.status(500).send(err);
-      console.log(confirmation);
-      res.json(data);
-    });
+    console.log(req.user);
   });
   app.get('/confirm', jwtauth, function(req, res) {
     newJob.find({}, function(err, data) {
