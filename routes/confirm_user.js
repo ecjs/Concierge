87ca4ed9dd23a900c1ef1cd42c3f4e2f5b96cc8b
Mark Harrell <mark@mjharrell.com>
2014-11-14 13:25:31 -0800

    confirmation wip

--- routes/confirm_user.js
+++ routes/confirm_user.js
@@ -5,6 +5,9 @@ module.exports = function(app, jwtauth) {
   app.post('/confirm', jwtauth, function(req, res) {
     var confirmation = req.body.confirmationCode;
     User.findOne({'_id': req.user._id, 'confirmationCode': confirmation}, function(err, user) {
+      if (user === null) {
+        return res.status(500).json({confirmed: false});
+      }
       if (err) { return next(err); }
       user.confirmed = true;
       user.save(function(err) {
