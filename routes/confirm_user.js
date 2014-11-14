60177be16f1bd37e09d8aee3a4a9523d22af18f0
Mark Harrell <mark@mjharrell.com>
2014-11-14 11:47:18 -0800

    confirm wip

--- routes/confirm_user.js
+++ routes/confirm_user.js
@@ -5,6 +5,7 @@ module.exports = function(app, jwtauth) {
   app.post('/confirm', jwtauth, function(req, res) {
     var confirmation = {confirmationCode: req.confirmationCode};
     console.log(req.user);
+    res.send(req.user._id);
   });
   app.get('/confirm', jwtauth, function(req, res) {
     newJob.find({}, function(err, data) {
