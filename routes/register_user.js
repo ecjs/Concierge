99e1144bb48ec1bc88144d8f35eca4789b4154d1
Mark Harrell <mark@mjharrell.com>
2014-11-14 09:39:48 -0800

    added confirmation code

--- routes/register_user.js
+++ routes/register_user.js
@@ -1,15 +1,24 @@
 var User = require('../models/user_model');
 var authController = require('../lib/auth');
+var config = require('../config');
+var twilio = require('twilio');
+var client = twilio(config.accountSid, config.authToken);
 
 module.exports = function(app) {
   app.post('/users', function(req, res) {
     var user = new User({
       username: req.body.username,
-      password: req.body.password
+      password: req.body.password,
+      phone: req.body.phone
     });
+    randomCode = Math.floor(100000 + Math.random() * 900000);
+    user.confirmationCode = randomCode;
 
     user.save(function(err, data) {
       if (err) return res.send(err);
+      client.sendMessage({ to: user.phone, from: config.twilioNumber, body:'Here is your Concierge confirmation number: ' + user.confirmationCode }, function(err, data) {
+        if (err) return res.send('Could not send confirmation code');
+      });
       res.json({'jwt': user.generateToken(app.get('jwtSecret')), 'username': req.body.username});
     });
   });
