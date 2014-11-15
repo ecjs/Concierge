9dd3c55fd45d8467aef295bc0c61c215df88c303
Mark Harrell <mark@mjharrell.com>
2014-11-14 18:32:02 -0800

    jobManager wip

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var authController = require('./lib/auth.js');
 var CronJob = require('cron').CronJob;
+var jobManager = require('./lib/jobManager');
 
 var app = express();
 var client = twilio(config.accountSid, config.authToken);
@@ -37,6 +38,6 @@ app.listen(config.port);
 
 console.log('Concierge magic starts here..on port: ' + config.port);
 
-// new CronJob('0 * * * * *', function() {
-//   console.log('You will see this message every minute');
-// }, null, true, "America/Los_Angeles");
+new CronJob('0 * * * * *', function() {
+  jobManager.checkJobs();
+}, null, true, "America/Los_Angeles");
