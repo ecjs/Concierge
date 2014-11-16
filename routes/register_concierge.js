7b219e703a019c797de4f468218f7ab9499c6a85
Mark Harrell <mark@mjharrell.com>
2014-11-15 21:20:45 -0800

    concierge wip

--- routes/register_concierge.js
+++ routes/register_concierge.js
@@ -5,6 +5,7 @@ var jwt = require('jsonwebtoken');
 module.exports = function(app, jwtauth) {
   app.post('/concierge', jwtauth, function(req, res) {
     console.log(req.user.username);
+    res.send(req.user.username);
     // User.findOneAndUpdate({username: req.username})
   });
   app.get('/concierge', function(req, res) {
