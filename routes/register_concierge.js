9d53fbc1223e40db4e3ea0f141afe6f0c2ea7693
Mark Harrell <mark@mjharrell.com>
2014-11-16 12:55:30 -0800

    concierge wip

--- routes/register_concierge.js
+++ routes/register_concierge.js
@@ -23,6 +23,24 @@ module.exports = function(app, jwtauth) {
       });
     });
   });
+  app.post('/conciergeAvailable', jwtauth, function(req, res) {
+    User.findOne({'_id': req.user._id}, function(err, user) {
+      if (err) {
+        console.log('error finding Concierge to make available: ' + err);
+        return res.status(500).json({message: 'error finding concierge'});
+      }
+      if (user === null) {
+        console.log('no user found matching that id');
+        return res.status(500).json({message: 'no user found matching that id'});
+      }
+      user.conciergeAvailable = true;
+      user.save(function(err) {
+        if (err) return res.status(500).json({message: 'no user found matching that id'});
+        console.log('successfully updated concierge to available: ' + user._id);
+        res.status(202).json({conciergeAvailable: true});
+      });
+    });
+  });
   app.get('/concierge', function(req, res) {
     res.json(req.body);
   });
