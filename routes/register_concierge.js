6567f8dab461cec9bff9f1581019d921103ce635
Mark Harrell <mark@mjharrell.com>
2014-11-15 21:45:19 -0800

    concierge wip

--- routes/register_concierge.js
+++ routes/register_concierge.js
@@ -4,12 +4,22 @@ var jwt = require('jsonwebtoken');
 
 module.exports = function(app, jwtauth) {
   app.post('/concierge', jwtauth, function(req, res) {
-    req.user.concierge = true;
-    User.findOneAndUpdate({'._id': req.user._id}, req.user, function(err, user) {
-      if (err) return console.log('error updating user to concierge: ' + err);
-      console.log('successfully updated user to concierge: ' + user);
+    User.findOne({'._id': req.user._id}, function(err, user) {
+      if (err) {
+        console.log('error finding user to add concierge: ' + err);
+        return res.status(500).json({message: 'error finding user'});
+      }
+      if (user === null) {
+        console.log('no user found matching that id');
+        return res.status(500).json({message: 'no user found matching that id'});
+      }
+      user.concierge = true;
+      user.save(function(err) {
+        if (err) return res.status(500).json({message: 'no user found matching that id'});
+        console.log('successfully updated user to concierge: ' + user);
+        res.status(202).json({concierge: true});
+      });
     });
-    res.send('successfully updated to concierge');
   });
   app.get('/concierge', function(req, res) {
     res.json(req.body);
