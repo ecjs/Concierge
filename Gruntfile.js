d8e005471121dddb748bd707df00af173d4aabb1
Joe Elsey <joe.elsey@gmail.com>
2014-11-18 12:41:02 -0800

    updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
         //ignoreLeaks: false,
         reporter: 'tap'
       },
-<<<<<<< HEAD
+
 
       src: ['test/user_test.js', /*'test/concierge_test.js',*/'test/jobs_test.js']
 
@@ -40,12 +40,12 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('test', ['jshint', 'jscs', 'simplemocha', /*'mongo_drop'*/]);
-=======
+
       src: ['test/user_test.js']
     }
   });
 
   grunt.registerTask('test', ['jshint', 'jscs', 'simplemocha']);
->>>>>>> upstream/dev
+
   grunt.registerTask('default',['test']);
 };
