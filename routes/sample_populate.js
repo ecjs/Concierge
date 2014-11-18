d63a330b3d93fd076b95761ca707dd01c7363c7c
Mark Harrell <mark@mjharrell.com>
2014-11-18 13:31:52 -0800

    populate wip

--- routes/sample_populate.js
+++ routes/sample_populate.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
     .send({username: 'test@example.com', password: 'foobar123', name: {first:'Mark', last: 'Harrell'}, phone: '5555555555'})
     .set('Content-Type', 'application/json')
     .end(function(err, superRes) {
-      if (err) return console.log('did not populate');
+      if (err) return console.log('did not populate' + err + " " + superRes);
       console.log('added user in populate' + res);
     });
   });
