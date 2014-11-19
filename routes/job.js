b5dd7b09d6d48da60b6bf4e530d6653cd0ef0339
Mark Harrell <mark@mjharrell.com>
2014-11-19 14:55:35 -0800

    job post wip

--- routes/job.js
+++ routes/job.js
@@ -6,12 +6,12 @@ var User = require('../models/user_model');
 module.exports = function(app, jwtauth) {
   app.post('/jobs', jwtauth, function(req, res) {
     console.log('the user for this job post is: ' + req.user);
-    console.log('the users name for this job post is: ' + req.user.name);
+    console.log('the users first name for this job post is: ' + req.user.name.first);
     var newJob = new Job({
       jobDate: req.body.jobDate,
       parent: req.user._id,
       recurring: req.body.recurring,
-      parentName: req.user.name,
+      parentName: {first: req.user.name.first, last: req.user.name.last},
       parentNumber: req.user.phone
     });
 
