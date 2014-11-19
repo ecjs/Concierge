3049f4f7e40b8eda0c8cb6e0ae84bb7a00167e88
Mark Harrell <mark@mjharrell.com>
2014-11-19 12:33:03 -0800

    user job delete wip

--- routes/user_jobs.js
+++ routes/user_jobs.js
@@ -12,20 +12,19 @@ module.exports = function(app, jwtauth) {
   });
 
   app.delete('/userJobs/:id', jwtauth, function(req, res) {
-    Job.remove({_id: req.params.id}, function(err, job) {
-      console.log(job);
+    Job.remove({_id: req.params.id}, function(err) {
       if (err) return res.status(500).send('there was an error deleting this job');
-      User.findOne({id_: job._parent}, function(err, user) {
-        var index = user.jobs.indexOf(job._id);
-        console.log(index);
-        if (index > -1) user.jobs.splice(index, 1);
-        console.log(user.jobs);
-        user.save(function(err, user) {
-          if (err) console.log('could not delete job from user array');
-          console.log(user);
-        });
-      });
       res.json({msg: 'Job deleted successfully!'});
+    });
+    User.findOne({id_: req.user._id}, function(err, user) {
+      var index = user.jobs.indexOf(req.params.id);
+      console.log(index);
+      if (index > -1) user.jobs.splice(index, 1);
+      console.log(user.jobs);
+      user.save(function(err, user) {
+        if (err) console.log('could not delete job from user array');
+        console.log(user);
+      });
     });
   });
 };
