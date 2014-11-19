7a7d37cb540f013ff1e0d6eaba0be8ddb38fd2f7
Mark Harrell <mark@mjharrell.com>
2014-11-18 22:25:49 -0800

    concierge wip

--- routes/register_concierge.js
+++ routes/register_concierge.js
@@ -74,7 +74,7 @@ module.exports = function(app, jwtauth) {
       }
       jobsArray = jobs.conciergeJobs;
       console.log(jobs);
-      jobQueue.find({id_: { $in: ['546c2d6c3fa04e02000f3ca5', '546c2d6c3fa04e02000f3ca6']}}, function(err, docs) {
+      jobQueue.find({'id_': { $in: ['546c2d6c3fa04e02000f3ca5', '546c2d6c3fa04e02000f3ca6']}}, function(err, docs) {
         if (err) return res.status(500).json({message: 'error finding concierge jobs'});
         if (jobs === null) return res.status(500).json({message: 'no jobs found for concierge'});
         res.status(200).send(docs);
