0eee0ab938ddfd758e2b05937f07f21f6c25252e
Mark Harrell <mark@mjharrell.com>
2014-11-19 13:04:20 -0800

    user job remove out of jobs array

--- routes/user_jobs.js
+++ routes/user_jobs.js
@@ -15,18 +15,15 @@ module.exports = function(app, jwtauth) {
     console.log(req.user._id);
     Job.remove({_id: req.params.id}, function(err) {
       if (err) return res.status(500).send('there was an error deleting this job');
-      res.json({msg: 'Job deleted successfully!'});
     });
     User.findOne({_id: req.user._id}, function(err, user) {
       if (user === null) console.log('not finding user');
-      console.log(user);
       var index = user.jobs.indexOf(req.params.id);
-      console.log(index);
       if (index > -1) user.jobs.splice(index, 1);
-      console.log(user.jobs);
       user.save(function(err, user) {
         if (err) console.log('could not delete job from user array');
         console.log(user);
+        res.json({msg: 'Job deleted successfully!'});
       });
     });
   });
