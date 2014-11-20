27a41aba77588233a10c3a641b3ac0f86f607d14
Mark Harrell <mark@mjharrell.com>
2014-11-20 11:43:06 -0800

    callback wip

--- routes/status_callback.js
+++ routes/status_callback.js
@@ -1,14 +1,29 @@
 'use strict';
 
-// var config = require('../config');
-// var twilio = require('twilio');
-// var client = twilio(config.accountSid, config.authToken);
+var config = require('../config');
+var twilio = require('twilio');
+var client = twilio(config.accountSid, config.authToken);
 
 module.exports = function(app) {
   app.post('/StatusCallBack/:firstName/:lastName/:phoneNumber', function(req, res) {
     if (req.body.AnsweredBy === 'machine') {
-      res.type('text/xml');
-      res.render('outboundMachine', {firstName: req.params.firstName, lastName: req.params.lastName, phoneNumber: req.params.phoneNumber});
+      client.makeCall({
+        to: req.params.phoneNumber,
+        from: config.twilioNumber,
+        // statusCallback: process.env.URL + '/StatusCallBack/' + req.params.firstName + '/' + req.params.lastName + '/' + req.params.phoneNumber,
+        statusCallbackMethod: 'POST',
+        ifMachine: 'Hangup',
+        url: process.env.URL + '/outboundMachine/' + req.params.firstName + '/' + req.params.lastName + '/'
+      }, function(err) {
+        console.log(err);
+        if (err) {
+          console.log('error making a call: ' + err);
+        }
+        else {
+          res.status(200).json({msg: 'call made'});
+          console.log('Secondary call successfully made!');
+        }
+      });
     }
   });
 };
