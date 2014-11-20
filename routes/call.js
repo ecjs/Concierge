a70040300c6279a0ad2a4dce5a4ffbe00a060e1b
Chareesa <chareesagraham@gmail.com>
2014-11-20 15:34:01 -0800

    CLEAN UP 1

--- routes/call.js
+++ routes/call.js
@@ -6,22 +6,20 @@ var client = twilio(config.accountSid, config.authToken);
 
 module.exports = function(app) {
   app.post('/call', function(req, res) {
-    console.log(req.body.phoneNumber);
     client.makeCall({
-            to: req.body.phoneNumber,
-            from: config.twilioNumber,
-            StatusCallBack: process.env.URL + '/StatusCallBack',
-            url: process.env.URL + '/outbound'
-        }, function(err) {
-          console.log(err);
-          if (err) {
-            res.status(500).send(err);
-          } else {
-            res.send({
-                    message: 'Thank you! We will be calling you shortly.'
-                });
-          }
+      to: req.body.phoneNumber,
+      from: config.twilioNumber,
+      StatusCallBack: process.env.URL + '/StatusCallBack',
+      url: process.env.URL + '/outbound'
+    }, function(err) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.send({
+          message: 'Thank you! We will be calling you shortly.'
         });
+      }
+    });
   });
 
   app.post('/outbound/:firstName/:lastName/:phoneNumber/', function(req, res) {
