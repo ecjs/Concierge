618f56a687f43e38208c4cfa9e97f86841109ea2
Mark Harrell <mark@mjharrell.com>
2014-11-20 19:39:11 -0800

    call status wip

--- routes/status_callback.js
+++ routes/status_callback.js
@@ -1,22 +1,16 @@
 'use strict';
 
-var config = require('../config');
-var twilio = require('twilio');
-var client = twilio(config.accountSid, config.authToken);
-var fallbackCall = require('../lib/fallBackCall');
+// var config = require('../config');
+// var twilio = require('twilio');
+// var client = twilio(config.accountSid, config.authToken);
+// var fallbackCall = require('../lib/fallBackCall');
 
 module.exports = function(app) {
   app.post('/StatusCallBack/:firstName/:lastName/:phoneNumber', function(req, res) {
     if ((req.body.AnsweredBy === 'machine') || (req.body.CallStatus === 'canceled' || 'failed' || 'no-answer')) {
-      client.calls(req.body.CallSid).update({
-        status: 'canceled'
-      }, function(err, call) {
-        if (err) console.log('error canceling call');
-        console.log(call.direction);
-        fallbackCall.makeCall(req.params.firstName, req.params.lastName, req.params.phoneNumber);
-        res.type('text/xml');
-        res.render('hangup');
-      });
+      res.type('text/xml');
+      res.render('hangup', {firstName: req.params.firstName, lastName: req.params.lastName, phoneNumber: req.params.lastName});
     }
+    console.log('call status is: ' + req.body.CallStatus);
   });
 };
