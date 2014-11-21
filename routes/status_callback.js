49eca2fa889f0182c30c880016114c32d33c5000
Mark Harrell <mark@mjharrell.com>
2014-11-20 23:00:34 -0800

    callback wip

--- routes/status_callback.js
+++ routes/status_callback.js
@@ -3,13 +3,14 @@
 // var config = require('../config');
 // var twilio = require('twilio');
 // var client = twilio(config.accountSid, config.authToken);
-// var fallbackCall = require('../lib/fallBackCall');
+var fallbackCall = require('../lib/fallBackCall');
 
 module.exports = function(app) {
   app.post('/StatusCallBack/:firstName/:lastName/:phoneNumber', function(req, res) {
     if ((req.body.AnsweredBy === 'machine') || (req.body.CallStatus === 'canceled' || req.body.CallStatus ===  'failed' || req.body.CallStatus ===  'no-answer')) {
       res.type('text/xml');
-      res.render('tryMachineAgain', {firstName: req.params.firstName, lastName: req.params.lastName, phoneNumber: req.params.phoneNumber});
+      res.render('hangupMachine');
+      fallbackCall.makeCall(req.params.firstName, req.params.lastName, req.params.phoneNumber);
     }
     console.log('call status is: ' + req.body.CallStatus);
   });
