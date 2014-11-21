a8c1d947c422437df9abf5eb4a597d8610c4c1e3
Mark Harrell <mark@mjharrell.com>
2014-11-20 22:31:51 -0800

    call status wip

--- routes/status_callback.js
+++ routes/status_callback.js
@@ -7,9 +7,9 @@
 
 module.exports = function(app) {
   app.post('/StatusCallBack/:firstName/:lastName/:phoneNumber', function(req, res) {
-    if ((req.body.AnsweredBy === 'machine') || (req.body.CallStatus === 'canceled' || 'failed' || 'no-answer')) {
+    if ((req.body.AnsweredBy === 'machine') || (req.body.CallStatus === 'canceled' || req.body.CallStatus ===  'failed' || req.body.CallStatus ===  'no-answer')) {
       res.type('text/xml');
-      res.render('hangup', {firstName: req.params.firstName, lastName: req.params.lastName, phoneNumber: req.params.phoneNumber});
+      res.render('tryMachineAgain', {firstName: req.params.firstName, lastName: req.params.lastName, phoneNumber: req.params.phoneNumber});
     }
     console.log('call status is: ' + req.body.CallStatus);
   });
