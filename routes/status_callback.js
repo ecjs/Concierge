2b0e3508cff5b3ab4a7525a98797cd37cd44ecd9
Mark Harrell <mark@mjharrell.com>
2014-11-20 16:03:48 -0800

    fail concierge call

--- routes/status_callback.js
+++ routes/status_callback.js
@@ -3,6 +3,7 @@
 var config = require('../config');
 var twilio = require('twilio');
 var client = twilio(config.accountSid, config.authToken);
+var fallbackCall = require('../lib/fallBackCall');
 
 module.exports = function(app) {
   app.post('/StatusCallBack/:firstName/:lastName/:phoneNumber', function(req, res) {
@@ -11,23 +12,8 @@ module.exports = function(app) {
         status: 'completed'
       }, function(err, call) {
         console.log(call.direction);
-      });
-      client.makeCall({
-        to: req.params.phoneNumber,
-        from: config.twilioNumber,
-        // statusCallback: process.env.URL + '/StatusCallBack/' + req.params.firstName + '/' + req.params.lastName + '/' + req.params.phoneNumber,
-        // statusCallbackMethod: 'POST',
-        ifMachine: 'Hangup',
-        url: process.env.URL + '/outboundMachine/' + req.params.firstName + '/' + req.params.lastName + '/'
-      }, function(err) {
-        console.log(err);
-        if (err) {
-          console.log('error making a call: ' + err);
-        }
-        else {
-          res.status(200).json({msg: 'call made'});
-          console.log('Secondary call successfully made!');
-        }
+        res.status(200).send('success');
+        fallbackCall.makeCall(req.params.firstName, req.params.lastName, req.params.phoneNumber);
       });
     }
   });
